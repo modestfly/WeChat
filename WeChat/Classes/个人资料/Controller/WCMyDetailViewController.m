cef5b16c6f12edc3d65680d64298d4cb44e1372c
modestfly <wangqifei10@163.com>
2018-08-23 17:10:53 +0800

    访问相册

--- WeChat/Classes/个人资料/Controller/WCMyDetailViewController.m
+++ WeChat/Classes/个人资料/Controller/WCMyDetailViewController.m
@@ -8,8 +8,7 @@
 
 #import "WCMyDetailViewController.h"
 
-@interface WCMyDetailViewController ()
-
+@interface WCMyDetailViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 @end
 
 @implementation WCMyDetailViewController
@@ -18,7 +17,71 @@
     [super viewDidLoad];
   
     self.navigationItem.title = @"我的";
+    UIButton *zhibo = [UIButton buttonWithType:UIButtonTypeCustom];
+    zhibo.frame = CGRectMake(100, 100, 100, 100);
+    zhibo.backgroundColor = [UIColor purpleColor];
+    [zhibo setTitle:@"头像" forState:UIControlStateNormal];
+    [zhibo addTarget:self action:@selector(zhiboAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:zhibo];
+
     // Do any additional setup after loading the view.
+}
+-(void)zhiboAction{
+    
+    UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"请选择" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *action1 = [UIAlertAction actionWithTitle:@"相册" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"点击了相册");
+        [self uploadImageFromPhotoAlbum];
+    }];
+    UIAlertAction *action2 = [UIAlertAction actionWithTitle:@"相机" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"点击了相机");
+    }];
+    UIAlertAction *action3 = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"点击了取消");
+    }];
+    [actionSheet addAction:action1];
+    [actionSheet addAction:action2];
+    [actionSheet addAction:action3];
+    [self presentViewController:actionSheet animated:YES completion:^{
+        
+    }];
+    
+}
+//访问相册
+-(void)uploadImageFromPhotoAlbum{
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        UIImagePickerController * imagePicker = [[UIImagePickerController alloc]init];
+        UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        imagePicker.delegate = self;
+        imagePicker.sourceType = sourceType;
+        imagePicker.allowsEditing = YES;
+    
+        [self presentViewController:imagePicker animated:YES completion:nil];
+    
+    } else {
+        NSLog(@"相册不管用啊");
+    }
+}
+#pragma mark -－ UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [picker dismissViewControllerAnimated:YES completion:^{
+        UIImage * image = [info objectForKey:UIImagePickerControllerEditedImage];
+        NSLog(@"%@",image);
+    }];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+
+}
+
+//访问摄像头
+-(void)uploadImageFromCamera{
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
